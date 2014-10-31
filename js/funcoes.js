5fa10dee079f0c842ac0b28c638e2c3da2a798b5
Caio Norder <caio@caionorder.com>
2014-10-31 21:32:42 -0200

    Add files
    º Parallax (Stellar)
    º Smooth-Scrooll
    º Correction WOW.js and add in Footer tags for loader.

--- js/funcoes.js
+++ js/funcoes.js
@@ -21,30 +21,12 @@ $(document).ready(function() {
     $("a[rel^='prettyPhoto']").prettyPhoto();
     $(".colorbox").colorbox({inline:true, width:"930px"});
 
-
-/* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
-            }
-            
-        }); 
-    
+    $.stellar({
+        responsive: true
     });
-
-    
+  
 
   });
-
+smoothScroll.init();
 // WOW Animations
-new WOW();
+new WOW().init();
